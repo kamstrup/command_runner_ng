5067422fe20e413796f2f0a75a6772fa80b2826a
Mikkel Kamstrup Erlandsen <mikkel.kamstrup@xamarin.com>
2015-09-16 10:40:17 +0200

    First working version. Unit tests included

--- lib/command_runner.rb
+++ lib/command_runner.rb
@@ -1,7 +1,101 @@
 module CommandRunner
 
-  def self.foo
-    puts "FOOOOO"
+  MAX_TIME = Time.new(2**63 -1)
+
+  # Like IO.spawn(), but block until the child completes.
+  # Takes an optional timeout parameter. If timeout is a
+  # number the child will be killed after that many seconds
+  # if it haven't completed. Alternatively it can be a Hash
+  # of timeouts to actions. Each action can be a string or integer
+  # specifying the signal to send, or a Proc to execute. The Proc
+  # will be called with the child PID as argument.
+  # These examples are equivalent:
+  #   run('sleep', '10', timeout: 5)
+  #   run('sleep', '10', timeout: {5 => 'KILL'})
+  #   run('sleep', '10', timeout: {5 => Proc.new { |pid| Process.kill('KILL', pid)}})
+  #   run('sleep', '10', timeout: {
+  #             5 => 'KILL',
+  #             2 = Proc.new {|pid| puts "PID #{pid} getting SIGKILL in 3s"}
+  #   })
+  #
+  # Returns a Hash with :out and :status. :out is a string with stdout
+  # and stderr merged, and :status is a Process::Status.
+  #
+  def self.run(*args, timeout: nil)
+    # These could be tweakable through vararg opts
+    tick = 0.1
+    bufsize = 4096
+
+    now = Time.now
+
+    # Build deadline_sequence. A list of deadlines and corresponding actions to take
+    if timeout
+      if timeout.is_a? Numeric
+        deadline_sequence = [{deadline: now + timeout, action: 'KILL'}]
+      elsif timeout.is_a? Hash
+        deadline_sequence = timeout.collect do |t, action|
+          unless action.is_a? Integer or action.is_a? String or action.is_a? Proc
+              raise "Unsupported action type '#{action.class}'. Must be Integer, String, or Proc"
+          end
+          unless t.is_a? Numeric
+            raise "Unsupported timeout value '#{t}'. Must be a Numeric"
+          end
+          {deadline: now + t, action: action}
+        end.sort! { |a, b| a[:deadline] <=> b[:deadline]}
+      else
+        raise "Unsupported type for timeout paramter: #{timeout.class}"
+      end
+    else
+      deadline_sequence = [{deadline: MAX_TIME, action: 0}]
+    end
+
+    # Spawn child, merging stderr into stdout
+    io = IO.popen(*args, :err=>[:child, :out])
+    data = ""
+
+    # Wait until stdout closes
+    while Time.now < deadline_sequence.first[:deadline] do
+      IO.select([io], nil, nil, tick)
+      begin
+        data << io.read_nonblock(bufsize)
+      rescue IO::WaitReadable
+        # Ignore: tick time reached without io
+      rescue EOFError
+        # Child closed stdout (probably dead, but not necessarily)
+        break
+      end
+    end
+
+    # Run through all deadlines until command completes
+    deadline_sequence.each do |point|
+      while Time.now < point[:deadline]
+        if Process.wait(io.pid, Process::WNOHANG)
+          result = {out: data, status: $?}
+          io.close
+          return result
+        else
+          sleep tick
+        end
+      end
+
+      # Deadline for this point reached. Fire the action.
+      action = point[:action]
+      if action.is_a? String or action.is_a? Integer
+        Process.kill(action, io.pid)
+      elsif action.is_a? Proc
+        action.call(io.pid)
+      else
+        # Given the assertions when building the deadline_sequence this should never be reached
+        raise "Internal error in CommandRunnerNG. Child may be left unattended!"
+      end
+    end
+
+    # Either we didn't have a deadline, or none of the deadlines killed of the child.
+    Process.wait(io.pid)
+    result = {out: data, status: $?}
+    io.close
+
+    result
   end
 
 end
---
+++ test/test_command_runner.rb
@@ -0,0 +1,51 @@
+require 'test/unit'
+require 'command_runner'
+
+class TestCommandRunner < Test::Unit::TestCase
+
+  def test_shell_echo
+    result = CommandRunner.run('echo hello')
+    assert_equal "hello\n", result[:out]
+    assert_equal 0, result[:status].exitstatus
+  end
+
+  def test_no_shell_echo
+    result = CommandRunner.run(['echo', 'hello'])
+    assert_equal "hello\n", result[:out]
+    assert_equal 0, result[:status].exitstatus
+  end
+
+  def test_shell_echo_sleep_timeout
+    result = CommandRunner.run('echo hello && sleep 5', timeout: 2)
+    assert_equal "hello\n", result[:out]
+    assert_equal 9, result[:status].termsig
+  end
+
+  def test_shell_echo_sleep_timeout_term
+    result = CommandRunner.run('echo hello && sleep 5', timeout: {2 => 'TERM'})
+    assert_equal "hello\n", result[:out]
+    assert_equal Signal.list['TERM'], result[:status].termsig
+  end
+
+  def test_shell_timeout_before_echo
+    result = CommandRunner.run('sleep 5; echo hello', timeout: {2 => 'QUIT'})
+    assert_equal "", result[:out]
+    assert Signal.list['QUIT'], result[:status].stopsig
+  end
+
+  def test_no_shell_timeout_procs_in_order
+    proc_callbacks = []
+
+    result = CommandRunner.run(['sleep', '10'], timeout: {
+                                   6 => Proc.new { proc_callbacks << 'proc6' },
+                                   1 => Proc.new { proc_callbacks << 'proc1' },
+                                   3 => Proc.new { proc_callbacks << 'proc3' },
+                                   5 => 'KILL',
+                                   2 => Proc.new { proc_callbacks << 'proc2' },
+                                      })
+    assert_equal "", result[:out]
+    assert_equal Signal.list['KILL'], result[:status].termsig
+    assert_equal ['proc1', 'proc2', 'proc3'], proc_callbacks
+  end
+
+end
